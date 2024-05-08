624484b47b1abefa5014864150d2ed6a2e8d0018
leoslopez <llopez@makingsense.com>
2024-05-08 12:50:41 -0300

    feat: update service worker

--- doppler-push-notification-sw.js
+++ doppler-push-notification-sw.js
@@ -1,22 +1,27 @@
 console.log('Service Worker Works');
 
 self.addEventListener('push', e => {
-    const data = e.data.json();
-    console.log(data)
     console.log('Notification Received');
+
+    const payload = e.data.json();
+    console.log(payload)
+
+    const options = {
+        body: payload.body,
+        image: payload.image || null,
+        icon: payload.icon || null,
+        data: payload.data || {},
+    };
+    console.log(options);
+
     self.registration.showNotification(
-        data.title,
-        {
-            body: data.body,
-            image: 'https://png.pngtree.com/element_origin_min_pic/16/08/05/1057a3fae73b91b.jpg',
-            icon: 'https://images.vexels.com/media/users/3/144131/isolated/preview/29576a7e0442960346703d3ecd6bac04-picture-doodle-icon.png',
-            data: data.data && data.data.messageId,
-        }
+        payload.title,
+        options,  
     );
 
     // TODO: it could invoke the API to inform messageId reception by the browser
-    if (data.data?.messageId) {
-        console.log(`pushing messageId: ${data.data.messageId}`);
+    if (payload.data?.messageId) {
+        console.log(`pushing messageId: ${payload.data.messageId}`);
     }
 });
 
@@ -24,13 +29,18 @@ self.addEventListener('notificationclick', (event) => {
     // Prevent the browser closing the notification automatically
     event.notification.close();
 
+    console.log(event.notification.data);
     // Perform some action based on the notification click
     // For example, redirect to a specific URL
-    const url = 'https://www.google.com';
-    event.waitUntil(
-        clients.openWindow(url)
-    );
+    if (event.notification.data?.clickLink) {
+        const url = event.notification.data.clickLink;
+        event.waitUntil(
+            clients.openWindow(url)
+        );
+    }
 
     // TODO: it could invoke the API to inform messageId clicked
-    console.log(`clicking messageId: ${event.notification.data}`);
+    if (event.notification.data?.messageId) {
+        console.log(`clicking messageId: ${event.notification.data.messageId}`);
+    }
 });
