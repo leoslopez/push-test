92fdd4d2dcd246ad9edc7092911d3a30ca62d4bb
leoslopez <llopez@makingsense.com>
2024-08-16 15:16:17 -0300

    chore: add worker generated using webpack

--- doppler-push-notification-sw.js
+++ doppler-push-notification-sw.js
@@ -1,62 +1,72 @@
-console.log('Service Worker Works (v1.0.0)');
+/******/ (() => { // webpackBootstrap
+/*!************************************************!*\
+  !*** ./public/doppler-push-notification-sw.js ***!
+  \************************************************/
+  console.log("Service Worker Works");
 
-function registerEvents(endpointUrl) {
+  function registerEvents(endpointUrl, eventType) {
     return fetch(endpointUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        }
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-    .then(response => {
+      .then((response) => {
         if (!response.ok) {
-            // TODO: handle logs properly
-            console.error('API response was not ok:', response.statusText);
+          // TODO: handle logs properly
+          console.error(
+            `API response was not ok: ${response.statusText}\nEventType:${eventType}\nEndpoint: ${endpointUrl}`,
+          );
         }
-    })
-    .catch(error => {
+      })
+      .catch((error) => {
         // TODO: handle logs properly
-        console.error('Failed to send click event to endpoint:', error);
-    });
-}
-
-self.addEventListener('push', (event) => {
-    console.log('Notification Received');
-
+        console.error(
+          `Failed registering event: ${error}\nEventType:${eventType}\nEndpoint: ${endpointUrl}`,
+        );
+      });
+  }
+  
+  self.addEventListener("push", (event) => {
+    console.log("Notification Received");
+  
     const payload = event.data.json();
     console.log(payload);
-
+  
     const options = {
-        body: payload.body,
-        image: payload.image || null,
-        icon: payload.icon || null,
-        data: payload.data || {},
+      body: payload.body,
+      image: payload.image || null,
+      icon: payload.icon || null,
+      data: payload.data || {},
     };
-    console.log(options);
-
+  
     self.registration.showNotification(payload.title, options);
-
+  
     // If receivedEventEndpoint is defined, make a fetch request to that endpoint
     if (payload.data?.receivedEventEndpoint) {
-        const endpointUrl = payload.data.receivedEventEndpoint;
-        event.waitUntil(registerEvents(endpointUrl));
+      const endpointUrl = payload.data.receivedEventEndpoint;
+      event.waitUntil(registerEvents(endpointUrl, "received"));
     }
-});
-
-self.addEventListener('notificationclick', (event) => {
+  });
+  
+  self.addEventListener("notificationclick", (event) => {
     // Prevent the browser closing the notification automatically
     event.notification.close();
-
+  
     console.log(event.notification.data);
-
+  
     // Redirect to a specific URL
     if (event.notification.data?.clickLink) {
-        const url = event.notification.data.clickLink;
-        event.waitUntil(clients.openWindow(url));
+      const url = event.notification.data.clickLink;
+      event.waitUntil(clients.openWindow(url));
     }
-
+  
     // If clickedEventEndpoint is defined, make a fetch request to that endpoint
     if (event.notification.data?.clickedEventEndpoint) {
-        const endpointUrl = event.notification.data.clickedEventEndpoint;
-        event.waitUntil(registerEvents(endpointUrl));
+      const endpointUrl = event.notification.data.clickedEventEndpoint;
+      event.waitUntil(registerEvents(endpointUrl, "clicked"));
     }
-});
+  });
+  
+  /******/ })()
+  ;
